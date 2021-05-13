03f02c4ff1ee4dbac4b375b7bc29a1ea97c5d078
Martín Montes <martin11lrx@gmail.com>
2021-05-13 20:39:57 +0200

    fix: start.sh

--- cron/start.sh
+++ cron/start.sh
@@ -3,13 +3,13 @@
 LOG="/var/log/photoprism-cron.log"
 CRONTAB="/photoprism/cron/crontab"
 
-if [ ! -z "$PHOTOPRISM_AUTO_INDEX_CRON" ]; then
+if [ "$PHOTOPRISM_AUTO_INDEX_CRON" = "" ]; then
   PHOTOPRISM_AUTO_INDEX_CRON="* 6 * * *"
 fi
 
 touch "$LOG"
 
-echo "$PHOTOPRISM_AUTO_INDEX_CRON /photoprism/cron/index.sh 2>> $LOG\n" > "$CRONTAB"
+echo "$PHOTOPRISM_AUTO_INDEX_CRON /photoprism/cron/index.sh 2>> $LOG\\n" > "$CRONTAB"
 crontab "$CRONTAB"
 
 /photoprism/cron/generate-cron.sh
